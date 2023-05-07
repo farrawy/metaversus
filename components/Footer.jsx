30094fc8be4480e01c3234319ca415d0a5dd5733
Ahmed Elfarra <ahmed.e.alfarra@gmail.com>
2023-05-07 23:43:49 +0300

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => (
         <div className="flex items-center justify-between flex-wrap gap-4">
           <h4 className="font-extrabold text-[24px] text-white">Metaversus</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © 2023 Metaversus. All rights reserved.
           </p>
           <div className="flex gap-4">
             {socials.map((social) => (
